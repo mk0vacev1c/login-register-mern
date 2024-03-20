d297e694493e87801ac1c67c4dc9224849738a2e
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-03-20 19:13:55 +0100

    project-setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+        Login-Register-App
+        </h1>
+    </div>
+  )
+}
