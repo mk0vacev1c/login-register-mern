692f51fc1644ec8fc93353baa78c139ce4857729
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-03-29 11:21:22 +0100

    Update appController.js

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -140,7 +140,27 @@ export async function login(req, res) {
 
 /** GET: http://localhost:8080/api/user/Milos123 */
 export async function getUser(req, res) {
-    res.json('getUser route');
+    const { username } = req.params;
+
+    try {
+        
+        if(!username) return res.status(501).send({ error: "Invalid Username"});
+
+        UserModel.findOne({ username }, function(err, user){
+            if(err) return res.status(500).send({ err });
+            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+
+            /** remove password from user */
+            // mongoose return unnecessary data with object so convert it into json
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+
+            return res.status(201).send(rest);
+        })
+
+    } catch (error) {
+        return res.status(404).send({ error : "Cannot Find User Data"});
+    }
+
 }
 
 
@@ -155,7 +175,27 @@ body: {
 }
 */
 export async function updateUser(req, res) {
-    res.json('updateUser route');
+    try {
+        
+        const { userId } = req.user;
+
+        if(userId){
+            const body = req.body;
+
+            // update the data
+            UserModel.updateOne({ _id : userId }, body, function(err, data){
+                if(err) throw err;
+
+                return res.status(201).send({ msg : "Record Updated...!"});
+            })
+
+        }else{
+            return res.status(401).send({ error : "User Not Found...!"});
+        }
+
+    } catch (error) {
+        return res.status(401).send({ error });
+    }
 }
 
 
