a841f0cf84a3d1867e388304e19c92bf56160b1a
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-03-29 11:46:14 +0100

    Update appController.js

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import { jwt } from 'jsonwebtoken';
 import ENV from '../config.js'
 
-
+/** Middleware for verify user */
 export async function verifyUser(req, res, next){
     try {
         
@@ -201,19 +201,29 @@ export async function updateUser(req, res) {
 
 /** GET: http://localhost:8080/api/generateOTP */
 export async function generateOTP(req, res) {
-    res.json('generateOTP route');
+    req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false})
+    res.status(201).send({ code: req.app.locals.OTP })
 }
 
 
 /** GET: http://localhost:8080/api/verifyOTP */
 export async function verifyOTP(req, res) {
-    res.json('verifyOTP route');
+    const { code } = req.query;
+    if(parseInt(req.app.locals.OTP) === parseInt(code)){
+        req.app.locals.OTP = null; // reset the OTP value
+        req.app.locals.resetSession = true; // start session for reset password
+        return res.status(201).send({ msg: 'Verify Successsfully!'})
+    }
+    return res.status(400).send({ error: "Invalid OTP"});
 }
 
 
 /** GET: http://localhost:8080/api/createResetSession */
 export async function createResetSession(req, res) {
-    res.json('createResetSession route');
+    if(req.app.locals.resetSession){
+        return res.status(201).send({ flag : req.app.locals.resetSession})
+   }
+   return res.status(440).send({error : "Session expired!"})
 }
 
 
