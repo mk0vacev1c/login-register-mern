5398fd10425288cbff241ce0d016fc1eae18a018
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-03-29 11:46:04 +0100

    auth-otp

--- server/router/route.js
+++ server/router/route.js
@@ -4,6 +4,7 @@ const router = Router();
 
 /** import all controllers */
 import * as controller from '../controllers/appController.js';
+import Auth, { localVariables } from "../middleware/auth.js";
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
@@ -13,12 +14,12 @@ router.route('/login').post(controller.verifyUser, controller.login); // login i
 
 /** Get Methods */
 router.route('/user/:username').get(controller.getUser); //user with username
-router.route('/generateOTP').get(controller.generateOTP); //generate random OTP
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP); //generate random OTP
 router.route('/verifyOTP').get(controller.verifyOTP); //verify generated OTP
 router.route('/createResetSession').get(controller.createResetSession); // reset all the variables
 
 /** Put Methods */
-router.route('/updateuser').put(controller.updateUser); // use to update the user prof
+router.route('/updateuser').put(Auth, controller.updateUser); // use to update the user prof
 router.route('/resetPassword').put(controller.resetPassword); // use to reset password
 
 export default router;
