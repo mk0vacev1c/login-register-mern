099d0bf1073f65ebd57c6dc9572a2d7a0cfd1b55
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-03-31 14:42:46 +0200

    axios

--- server/router/route.js
+++ server/router/route.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 const router = Router();
+import { registerMail } from "../controllers/mailer.js";
+import Auth, { localVariables } from "../middleware/auth.js";
 
 
 /** import all controllers */
@@ -11,7 +13,7 @@ import Auth, { localVariables } from "../middleware/auth.js";
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
 router.route('/registerMail').post(registerMail); // send the email
-router.route('/authenticate').post((req, res) => res.end()); // authenticate user
+router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser, controller.login); // login in app
 
 /** Get Methods */
