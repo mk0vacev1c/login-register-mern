f162331c32c8e1fd37279b90dcfdde95f8d75780
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-04-01 13:12:01 +0200

    custom-hook

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -4,10 +4,16 @@ import avatar from '../assets/profile.png';
 import {Toaster} from 'react-hot-toast';
 import {useFormik} from 'formik';
 import { passwordValidate } from '../helper/validate';
+import useFetch from '../hooks/fetch.hook';
+import { useAuthStore } from '../store/store'
 
 import styles from '../styles/Username.module.css';
 
 export default function Password() {
+
+  const { username } = useAuthStore(state => state.auth)
+
+  const [{ isLoading, apiData, serverError }]= useFetch(`/user/${username}`)
 
   const formik = useFormik({
     initialValues : {
@@ -21,6 +27,9 @@ export default function Password() {
     }
   })
 
+  if(isLoading) return <h1 className='text-2xl font-bold'>isLoading</h1>
+  if(serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1>
+
   return (
     <div className='container mx-auto'>
 
@@ -30,7 +39,7 @@ export default function Password() {
 
         <div className={styles.glass}>
          <div className='title flex flex-col items-center'>
-          <h4 className='text-5xl font-bold'>Hello</h4>
+          <h4 className='text-5xl font-bold'>Hello {apiData?.firstName || apiData?.username}</h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
               Explore more.
             </span>
@@ -38,7 +47,7 @@ export default function Password() {
 
             <form className='py-1'onSubmit={formik.handleSubmit} >
               <div className='profile flex justify-center py-4'>
-                <img src={avatar} className={styles.profile_img} alt="avatar" />
+                <img src={apiData?.profile || avatar} className={styles.profile_img} alt="avatar" />
               </div>
 
               <div className='textbox flex flex-col items-center gap-6'>
